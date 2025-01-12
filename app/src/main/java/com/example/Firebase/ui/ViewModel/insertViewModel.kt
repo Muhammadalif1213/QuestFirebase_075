301cb5febb1787cce2d093bb23caa3935acc1bce
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-12 22:54:43 +0700

    Menambahkan fungsi toMhsModel untuk konversi dari MahasiswaEvent ke model Mahasiswa.

--- app/src/main/java/com/example/Firebase/ui/ViewModel/insertViewModel.kt
+++ app/src/main/java/com/example/Firebase/ui/ViewModel/insertViewModel.kt
@@ -1,5 +1,16 @@
 package com.example.Firebase.ui.ViewModel
 
+import com.example.Firebase.model.Mahasiswa
+
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenis_kelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
 data class MahasiswaEvent(
     val nim: String = "",
     val nama: String = "",
