d763cc74f4415245f5ff4cbd30fdf0bce6cc6740
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-12 22:52:36 +0700

    Menambahkan dataclass MahasiswaEvent di insertViewModel.

---
+++ app/src/main/java/com/example/Firebase/ui/ViewModel/insertViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.Firebase.ui.ViewModel
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
