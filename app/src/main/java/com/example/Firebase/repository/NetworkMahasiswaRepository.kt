e3aede35124cbf6d8caf1b4ec0b4e54b2ef94c41
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-12 14:04:06 +0700

    menambahkan fungsi getMahasiswaByNim di NetworkMahasiswaRepository

--- app/src/main/java/com/example/Firebase/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/Firebase/repository/NetworkMahasiswaRepository.kt
@@ -62,7 +62,18 @@ class NetworkMahasiswaRepository (
         }
     }
 
-    override suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa> {
-        TODO("Not yet implemented")
+    override suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa> = callbackFlow {
+        val mhsDocument = firestore.collection("Mahasiswa")
+            .document(nim)
+            .addSnapshotListener{ value, error ->
+                if (value != null) {
+                    val mhs = value.toObject(Mahasiswa::class.java)!!
+                    trySend(mhs)
+                }
+            }
+        awaitClose{
+            mhsDocument.remove()
+        }
+
     }
 }
