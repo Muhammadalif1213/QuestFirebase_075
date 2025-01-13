d8d4c9bfd734b568553b27483d1437f2a51a9635
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-13 11:57:14 +0700

    Menambahkan NavHost untuk mengatur navigasi antar halaman dengan startDestination yang ditetapkan ke DestinasiHome.route.

--- app/src/main/java/com/example/Firebase/ui/Navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/Firebase/ui/Navigation/PengelolaHalaman.kt
@@ -2,7 +2,9 @@ package com.example.Firebase.ui.Navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 @Composable
@@ -10,5 +12,10 @@ fun PengelolaHalaman(
     modifier: Modifier,
     navController: NavHostController = rememberNavController()
 ) {
-
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+    }
 }
