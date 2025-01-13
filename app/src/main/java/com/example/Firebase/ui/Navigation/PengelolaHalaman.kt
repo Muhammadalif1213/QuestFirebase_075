c8cb0fb81f881d0656e23936a2257f8e95084f4f
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-13 11:55:47 +0700

    Menambahkan fungsi PengelolaHalaman untuk mengatur navigasi dalam aplikasi menggunakan NavHostController.

---
+++ app/src/main/java/com/example/Firebase/ui/Navigation/PengelolaHalaman.kt
@@ -0,0 +1,14 @@
+package com.example.Firebase.ui.Navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+
+}
